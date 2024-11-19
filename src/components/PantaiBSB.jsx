c18594bd3628f87f4bdbb2726097f2c2c15dd21b
Ardi11221026 <ardi11221026@example.com>
2024-11-19 16:35:05 +0800

    update

--- src/components/PantaiBSB.jsx
+++ src/components/PantaiBSB.jsx
@@ -29,7 +29,7 @@ const PantaiBSB = () => {
                         />
 
                         <p className="leading-relaxed max-w-[900px] mb-8 text-justify">
-                            Pantai BSB terletak di Balikpapan, Kalimantan Timur. Pantai ini merupakan salah satu destinasi wisata yang populer di kalangan wisatawan lokal dan mancanegara. Pantai BSB menawarkan pemandangan laut yang indah, cocok untuk bersantai, menikmati sunset, atau berolahraga di tepi pantai.
+                        Pantai BSB, yang terletak di Balikpapan, Kalimantan Timur, merupakan destinasi wisata populer yang menarik perhatian wisatawan lokal maupun mancanegara. Pantai ini menawarkan pemandangan laut yang memukau dengan suasana yang tenang, menjadikannya tempat yang ideal untuk bersantai, menikmati keindahan matahari terbenam, atau melakukan berbagai aktivitas seperti jogging dan bermain voli pantai. Selain itu, fasilitas pendukung seperti area parkir, warung makan, dan tempat duduk tepi pantai membuat kunjungan ke Pantai BSB semakin nyaman. Dengan akses yang mudah dari pusat kota, pantai ini menjadi pilihan favorit untuk menghabiskan waktu bersama keluarga atau teman.
                         </p>
 
                         <h2 className="text-left mb-5 text-2xl font-semibold">Aktivitas dan Fasilitas</h2>
@@ -39,7 +39,7 @@ const PantaiBSB = () => {
 
                         <h2 className="text-left mb-5 text-2xl font-semibold">Akses dan Lokasi</h2>
                         <p className="leading-relaxed max-w-[900px] mb-8 text-justify">
-                            Pantai BSB dapat diakses dengan mudah menggunakan kendaraan pribadi atau transportasi umum. Lokasinya yang strategis di Balikpapan membuat pantai ini menjadi salah satu tujuan favorit bagi warga kota dan wisatawan.
+                        Pantai BSB mudah diakses menggunakan kendaraan pribadi maupun transportasi umum, menjadikannya salah satu destinasi wisata yang paling diminati di Balikpapan. Berlokasi strategis tidak jauh dari pusat kota, pantai ini dapat dicapai dalam waktu singkat dengan jalur yang sudah terhubung baik. Popularitasnya sebagai tujuan rekreasi tidak hanya menarik perhatian wisatawan dari luar daerah, tetapi juga menjadi tempat favorit warga lokal untuk bersantai atau berolahraga di akhir pekan. Dengan fasilitas yang memadai, seperti area parkir, warung makan, dan tempat istirahat, Pantai BSB menawarkan kenyamanan bagi semua kalangan pengunjung.
                         </p>
                     </div>
 
